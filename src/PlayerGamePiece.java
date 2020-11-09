708178733b99b3be8daede985264ec19c147e507
Mark Schroeder <mark@4schroeders.com>
2020-11-08 22:46:49 -0500

    Initial game design.

---
+++ src/PlayerGamePiece.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics;
+
+public class PlayerGamePiece extends GamePiece {
+
+	public PlayerGamePiece(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
